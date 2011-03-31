8bed0ae82f731607f43ad9874d4d4d5d58951bb0
sol <sol@d8022a0d-75f1-32e2-6f5f-92e66f081be6>
2011-03-31 00:15:43 +0000

    init branches 1.2.0

---
+++ branches/modules/1.2.0/skins/xe_project/js/toggle.js
@@ -0,0 +1,24 @@
+jQuery(function($){
+	// Input Clear
+	var iText = $('.item>.iLabel').next('.iText');
+	$('.item>.iLabel').css('position','absolute');
+	iText
+		.focus(function(){
+			$(this).prev('.iLabel').css('visibility','hidden');
+		})
+		.blur(function(){
+			if($(this).val() == ''){
+				$(this).prev('.iLabel').css('visibility','visible');
+			} else {
+				$(this).prev('.iLabel').css('visibility','hidden');
+			}
+		})
+		.change(function(){
+			if($(this).val() == ''){
+				$(this).prev('.iLabel').css('visibility','visible');
+			} else {
+				$(this).prev('.iLabel').css('visibility','hidden');
+			}
+		})
+		.blur();
+});
